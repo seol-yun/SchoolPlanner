e31cc9a43bee1a5083d7a9ec1fb02ee339c88592
yunHwan Seol <dyj07132@gmail.com>
2024-11-20 19:45:12 +0900

    깃액션 배포 , mysql

--- src/main/java/schoolplan/schoolplanner/repository/LectureEnrollmentRepository.java
+++ src/main/java/schoolplan/schoolplanner/repository/LectureEnrollmentRepository.java
@@ -7,7 +7,7 @@ import java.util.List;
 import java.util.Optional;
 
 public interface LectureEnrollmentRepository extends JpaRepository<LectureEnrollment, Long> {
-    //배포6
+    //배포7
     // 특정 회원의 연도와 학기에 해당하는 수강신청 내역 조회
     List<LectureEnrollment> findByMember_IdAndLecture_OpenYearAndLecture_Semester(String memberId, String openYear, String semester);
 }
